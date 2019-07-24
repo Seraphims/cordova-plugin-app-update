212374f84cce4db89fefa29cdccfbc16c5e206e5
seraphim <751701226@qq.com>
2019-07-24 11:12:44 +0800

    设置setRequestProperty

--- src/android/DownloadApkThread.java
+++ src/android/DownloadApkThread.java
@@ -72,6 +72,7 @@ public class DownloadApkThread implements Runnable {
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
                 //加上下面,后端要设置response.setContentLengthLon,要不total为-1
+                conn.setRequestProperty("Accept-Encoding", "identity");
                 conn.setRequestProperty("User-Agent", " Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36");
 
                 if(this.authentication.hasCredentials()){
